e45f6ce62144440c12aef8019f3240311b5c2765
Kurt <kurt@debpc>
2018-02-06 00:29:06 +0100

    added the shell script

---
+++ BashBackground.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+WP_DIR=/home/kurt/Pictures/Wallpapers
+
+cd $WP_DIR
+while [ 1 ] 
+  do
+  set -- * 
+  length=$#
+  random_num=$((( $RANDOM % ($length) ) + 1)) 
+
+  gsettings set org.gnome.desktop.background picture-uri "file://$WP_DIR/${!random_num}"
+
+  sleep 600 
+done
+
