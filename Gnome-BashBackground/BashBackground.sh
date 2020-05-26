4fc4d06141442f147abf968e7a165250f549a40b
KurtC0ba1n <romain.escrime@gmail.com>
2020-05-26 18:04:58 +0200

    fully rewritting the script to work on top of Cron

--- Gnome-BashBackground/BashBackground.sh
+++ Gnome-BashBackground/BashBackground.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+wall_dir="/home/$USER/Pictures/Wallpapers"
+printf "starting the installation...\n the default directory to put your wallpapers is $wall_dir \n"
+read -p "do you want to change it ? (y for yes, n for no)" change_dir
+if [ $change_dir = 'y' ];then
+  read -p "Enter the absolute path directory where you want to put you Wallpapers(leave empty for the default /home/$USER/Pictures/Wallpapers): " wall_dir
+fi
 
-WP_DIR=~/Pictures/Wallpapers
+read -p "enter a changing interval in minutes(between 0 and 59): " min
 
-cd $WP_DIR
-while [ 1 ] 
-  do
-  set -- * 
-  length=$#
-  random_num=$((( $RANDOM % ($length) ) + 1)) 
+if [ ! -d $wall_dir ];then
+  mkdir $wall_dir
+  printf "a directory was created at $wall_dir"
+fi
 
-  gsettings set org.gnome.desktop.background picture-uri "file://$WP_DIR/${!random_num}"
 
-  sleep 600 
-done
+(crontab -l 2>/dev/null; echo '*/'$min' * * * * PID=$(pgrep gnome-session | tail -n1 ) && export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-) && gsettings set org.gnome.desktop.background picture-uri "file://"$(find '$wall_dir' -type f | shuf -n 1)') | crontab -
 
+printf "installation done !\n"
